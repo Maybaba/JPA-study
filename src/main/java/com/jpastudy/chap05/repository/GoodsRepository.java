4a39471dcb4ea1cdcd58407278094547929f8a3b
Maybaba <sinyunjong@gmail.com>
2024-06-27 10:58:49 +0900

    # query DSL settings

--- src/main/java/com/jpastudy/chap05/repository/GoodsRepository.java
+++ src/main/java/com/jpastudy/chap05/repository/GoodsRepository.java
@@ -3,7 +3,10 @@ package com.jpastudy.chap05.repository;
 import com.jpastudy.chap05.entity.Goods;
 import com.jpastudy.chap05.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GoodsRepository extends JpaRepository<Goods, Long> {
+
+
 }
 
